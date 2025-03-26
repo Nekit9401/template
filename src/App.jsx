865e4c6570a3bd50b5f8c6463938713ca107d960
Nikita <nekit9401@yandex.ru>
2025-03-26 12:27:30 +0300

    add sort

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,33 @@ import { DeleteButton } from './component/DeleteButton';
 
 export const App = () => {
 	const [dataUsers, setDataUsers] = useState(users);
+	const [sortBy, setSortBy] = useState({ key: 'name', order: 'asc' });
+
+	const handleSortUsers = (key) => {
+		if (sortBy.key === key) {
+			setSortBy({
+				...sortBy,
+				order: sortBy.order === 'asc' ? 'desc' : 'asc',
+			});
+		} else {
+			setSortBy({
+				...sortBy,
+				key: key,
+				order: sortBy.order === 'asc' ? 'desc' : 'asc',
+			});
+		}
+	};
+
+	const sortedUsers = [...dataUsers].sort((a, b) => {
+		const valueA = a[sortBy.key];
+		const valueB = b[sortBy.key];
+
+		if (typeof valueA === 'number') {
+			return sortBy.order === 'asc' ? valueA - valueB : valueB - valueA;
+		} else {
+			return sortBy.order === 'asc' ? valueA.localeCompare(valueB) : valueB.localeCompare(valueA);
+		}
+	});
 
 	const renderContent = (id, user, column) => {
 		if (column === 'delete') {
@@ -19,12 +46,14 @@ export const App = () => {
 				<thead>
 					<tr>
 						{Object.keys(table).map((column) => (
-							<th key={column}>{table[column].title}</th>
+							<th key={column} onClick={() => handleSortUsers(table[column].key)}>
+								{table[column].title}
+							</th>
 						))}
 					</tr>
 				</thead>
 				<tbody>
-					{dataUsers.map((user) => (
+					{sortedUsers.map((user) => (
 						<tr key={user._id}>
 							{Object.keys(table).map((column) => (
 								<td key={column}>{renderContent(user._id, user, column)}</td>
