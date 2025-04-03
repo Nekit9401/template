02db7ff0452ba99e0ef0b5c4ec715f9ebed5432e
Nikita <nekit9401@yandex.ru>
2025-04-03 10:09:44 +0300

    finish todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,32 @@
-// import { useState } from 'react';
-// import styles from 'App.module.css';
+import { useEffect, useState } from 'react';
+import styles from './App.module.css';
 
 export const App = () => {
-	return <></>;
+	const [todoData, setTodoData] = useState([]);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+				const loadedTodoData = await response.json();
+				setTodoData(loadedTodoData);
+			} catch (error) {
+				console.error(error);
+			}
+		};
+
+		fetchData();
+	}, []);
+
+	return (
+		<>
+			<div className={styles.todoList}>
+				{todoData.map(({ id, title }) => (
+					<div key={id} className={styles.todoItem}>
+						{title}
+					</div>
+				))}
+			</div>
+		</>
+	);
 };
