65304f6d72d27c7a9c1b0db3349b6cda4c8ab79e
Nikita <nekit9401@yandex.ru>
2025-04-06 15:36:59 +0300

    Finish todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,165 @@
 import { useEffect, useState } from 'react';
 import styles from './App.module.css';
+import { TodoItem } from './components/TodoItem';
 
 export const App = () => {
 	const [todoData, setTodoData] = useState([]);
+	const [inputValue, setInputValue] = useState('');
+	const [searchValue, setSearchValue] = useState('');
+	const [error, setError] = useState('');
+	const [isLoading, setIsLoading] = useState(false);
+	const [isSorted, setIsSorted] = useState(false);
+
+	const fetchData = async () => {
+		setIsLoading(true);
+		try {
+			const response = await fetch('http://localhost:3000/todos');
+			const loadedTodoData = await response.json();
+			setTodoData(loadedTodoData);
+			setIsSorted(false);
+		} catch (error) {
+			setError(error.message);
+		} finally {
+			setIsLoading(false);
+		}
+	};
+
+	const addTodo = async (payLoad) => {
+		setIsLoading(true);
+		try {
+			const response = await fetch('http://localhost:3000/todos', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(payLoad),
+			});
+			const addsTodo = await response.json();
+			setTodoData([...todoData, addsTodo]);
+			setInputValue('');
+		} catch (error) {
+			setError(error.message);
+		} finally {
+			setIsLoading(false);
+		}
+	};
+
+	const deleteTodo = async (id) => {
+		setIsLoading(true);
+		try {
+			fetch(`http://localhost:3000/todos/${id}`, {
+				method: 'DELETE',
+			});
+			setTodoData(todoData.filter((todo) => todo.id !== id));
+		} catch (error) {
+			setError(error.message);
+		} finally {
+			setIsLoading(false);
+		}
+	};
+
+	const updateTodo = async (id, payLoad) => {
+		setIsLoading(true);
+		try {
+			const response = await fetch(`http://localhost:3000/todos/${id}`, {
+				method: 'PATCH',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(payLoad),
+			});
+			const updatedTodo = await response.json();
+			setTodoData((prevData) => prevData.map((todo) => (todo.id === id ? { ...todo, ...updatedTodo } : todo)));
+		} catch (error) {
+			setError(error.message);
+		} finally {
+			setIsLoading(false);
+		}
+	};
+
+	const sortTodo = async () => {
+		setIsLoading(true);
+		try {
+			const response = await fetch('http://localhost:3000/todos?_sort=title');
+			const sortedTodoData = await response.json();
+			setTodoData(sortedTodoData);
+			setIsSorted(true);
+		} catch (error) {
+			setError(error.message);
+		} finally {
+			setIsLoading(false);
+		}
+	};
+
+	const searchTodo = async (phrase) => {
+		setIsLoading(true);
+		try {
+			const response = await fetch(`http://localhost:3000/todos?title_like=${phrase}`);
+			const filteredTodoData = await response.json();
+			setTodoData(filteredTodoData);
+			setSearchValue('');
+		} catch (error) {
+			setError(error.message);
+		} finally {
+			setIsLoading(false);
+		}
+	};
+
+	const handleCreateSubmit = (event) => {
+		event.preventDefault();
+		addTodo({ title: inputValue, complete: false });
+	};
+
+	const handleSearchSubmit = (event) => {
+		event.preventDefault();
+		searchTodo(searchValue);
+	};
 
 	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const response = await fetch('https://jsonplaceholder.typicode.com/todos');
-				const loadedTodoData = await response.json();
-				setTodoData(loadedTodoData);
-			} catch (error) {
-				console.error(error);
-			}
-		};
-
 		fetchData();
 	}, []);
 
+	if (isLoading) {
+		return <h1>Загрузка...</h1>;
+	}
+	if (error) {
+		return <h2>{error}</h2>;
+	}
+
 	return (
-		<>
-			<div className={styles.todoList}>
-				{todoData.map(({ id, title }) => (
-					<div key={id} className={styles.todoItem}>
-						{title}
-					</div>
-				))}
+		<div className={styles.container}>
+			<div>
+				<form onSubmit={handleCreateSubmit}>
+					<input
+						name='create'
+						type='text'
+						value={inputValue}
+						onChange={(e) => setInputValue(e.target.value)}
+					/>
+					<button className={styles.createTodoButton} type='submit'>
+						Создать задачу
+					</button>
+				</form>
+				<form onSubmit={handleSearchSubmit}>
+					<input
+						name='search'
+						type='text'
+						value={searchValue}
+						onChange={(e) => setSearchValue(e.target.value)}
+					/>
+					<button className={styles.searchTodoButton} type='submit'>
+						Найти
+					</button>
+				</form>
+				<button className={styles.sortTodoButton} onClick={isSorted ? fetchData : sortTodo}>
+					{isSorted ? 'Сортировать по умочанию' : 'Сортировать по алфавиту'}
+				</button>
 			</div>
-		</>
+			<ul className={styles.todoList}>
+				{todoData.map((todo) => (
+					<TodoItem key={todo.id} {...todo} deleteTodo={deleteTodo} updateTodo={updateTodo} />
+				))}
+			</ul>
+		</div>
 	);
 };
---
+++ src/components/TodoItem/TodoItem.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import styles from './TodoItem.module.css';
+
+export const TodoItem = ({ id, title, deleteTodo, updateTodo }) => {
+	const [isEditing, setIsEditing] = useState(false);
+	const [text, setText] = useState(title);
+
+	const handleEdit = () => {
+		setIsEditing(!isEditing);
+	};
+
+	const handleSave = (event) => {
+		event.preventDefault();
+		updateTodo(id, { title: text });
+	};
+
+	return (
+		<div>
+			{isEditing ? (
+				<form onSubmit={handleSave}>
+					<input name='edit' type='text' value={text} onChange={(e) => setText(e.target.value)} />
+					<button className={styles.saveButton} type='submit'>
+						Сохранить
+					</button>
+					<button type='button' className={styles.cancelButton} onClick={handleEdit}>
+						Отменить
+					</button>
+				</form>
+			) : (
+				<div className={styles.todoItem}>
+					<h1>{title}</h1>
+					<div className={styles.actionButton}>
+						<button className={styles.deleteButton} onClick={() => deleteTodo(id)}>
+							Удалить
+						</button>
+						<button className={styles.editButton} onClick={handleEdit}>
+							Редактировать
+						</button>
+					</div>
+				</div>
+			)}
+		</div>
+	);
+};
